10b36b2cf3f177bf3eb4f03cc78d98d68fb86f89
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-09-02 02:18:40 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,18 @@
-def oxford_comma(array)
-a = ["kiwi"]
-a.join
 
-a << "durian"
-a.join(" and ")
+def oxford_comma(array)
+  if array.length == 1 
+  array.join('')
+  
+  elsif array.length == 2 
+    array.join(' and ')
+    elsif array.length >= 3 
+      array[-1] = "and #{array[-1]}"
+      array.join(', ')
+    else 
+      nil 
+    end 
+end   
+  
 
   
 
